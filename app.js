e09ae910ffb879d005fda4349d9a79af2f49172d
luffy-hh <heinhtetsoe2566@gmail.com>
2024-09-16 12:06:12 +0630

    added public folder

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json()); // Parses JSON bodies
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(express.static("public"));
 app.use((req, res, next) => {
   //   console.log(req.headers);
   next();
