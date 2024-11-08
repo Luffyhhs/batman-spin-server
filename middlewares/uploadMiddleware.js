157caf9b464c574ac433a606a970250591bf91b2
luffy-hh <heinhtetsoe2566@gmail.com>
2024-11-09 00:49:19 +0630

    add needed /

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -69,7 +69,7 @@ const uploadToSpace = async (req, res, next) => {
       req.uploadedFile = {
         Key: uploadedFile.Key,
         Bucket: uploadedFile.Bucket,
-        Location: `${process.env.DO_SPACE_REG_ENDPOINT}/${process.env.DO_SPACE_BUCKET}${uploadedFile.Key}`,
+        Location: `${process.env.DO_SPACE_REG_ENDPOINT}/${process.env.DO_SPACE_BUCKET}/${uploadedFile.Key}`,
       };
       // console.log(req.uploadedFile, "line 74");
       next();
